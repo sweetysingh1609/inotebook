f61c5c1144f1a77e9099d54fdf68040f2bb43d73
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-01-27 13:49:15 +0530

    Adding Data Validation Using express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+  "mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+// const mongoURI = "mongodb+srv://sweety:Sweety@123@cluster0.gbkbvfz.mongodb.net/?retryWrites=true&w=majority";
 
   const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
