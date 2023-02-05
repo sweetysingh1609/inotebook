16afe14be32dda6bef158e32404a5bc9256abd93
sweetysingh1609 <sweetysingh1609@gmail.com>
2023-02-05 13:24:53 +0530

    Implemented Routes and Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
-
+import { Router, Switch, Route } from "react-router-dom";
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from "./components/About";
+import { createBrowserHistory } from "history";
+export const customHistory = createBrowserHistory();
 function App() {
   return (
-    <div className="App">
-     <h1>React app</h1>
-    </div>
+    <>
+      <Router history={customHistory}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
